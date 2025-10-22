ce115b796d056ee5043951944952ba6013a148cb
Mostafa Ali <ma0387292@gmail.com>
2025-10-22 16:50:34 +0300

    Updated port configuration for Render deployment

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
